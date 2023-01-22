90346807904aaedfd96165a302aa17aef79ec441
Osamu Sakai <fumumue@protonmail.com>
2023-01-23 05:06:58 +0900

    select_gf

--- mine/lyon.c
+++ mine/lyon.c
@@ -613,6 +613,34 @@ int ben_or(vec f)
     return 0;
 }
 
+unsigned short gf_mul(unsigned short in0, unsigned short in1)
+{
+    int i;
+
+    uint32_t tmp;
+    uint32_t t0;
+    uint32_t t1;
+    uint32_t t;
+
+    t0 = in0;
+    t1 = in1;
+
+    tmp = t0 * (t1 & 1);
+
+    for (i = 1; i < 12; i++)
+        tmp ^= (t0 * (t1 & (1 << i)));
+
+    t = tmp & 0x7FC000;
+    tmp ^= t >> 9;
+    tmp ^= t >> 12;
+
+    t = tmp & 0x3000;
+    tmp ^= t >> 9;
+    tmp ^= t >> 12;
+
+    return tmp & ((1 << 12) - 1);
+}
+
 /* input: in0, in1 in GF((2^m)^t)*/
 /* output: out = in0*in1 */
 void GF_mul(unsigned short *out, unsigned short *in0, unsigned short *in1)
@@ -633,68 +661,68 @@ void GF_mul(unsigned short *out, unsigned short *in0, unsigned short *in1)
 
     for (i = (K - 1) * 2; i >= K; i--)
     {
-        /*
-        //GF(2^1024) from sage
-        prod[i - K + 19] ^= prod[i];
-        prod[i - K + 6] ^= prod[i];
-        prod[i - K + 1] ^= prod[i];
-        prod[i - K + 0] ^= prod[i];
-        */
-        /*
-            //GF(2^512) from sage
+        if (K == 512)
+        {
+            // GF(2^512) from sage
             prod[i - K + 8] ^= prod[i];
             prod[i - K + 5] ^= prod[i];
             prod[i - K + 2] ^= prod[i];
             prod[i - K + 0] ^= prod[i];
-          */
-         /*
-        // GF(2^256) from sage
-        prod[i - K + 10] ^= prod[i];
-        prod[i - K + 5] ^= prod[i];
-        prod[i - K + 2] ^= prod[i];
-        prod[i - K + 0] ^= prod[i];
-        */
-        /*    //  GF(2^16) sage
-        prod[i - K + 5] ^= prod[i];
-        prod[i - K + 3] ^= prod[i];
-        prod[i - K + 2] ^= prod[i];
-        prod[i - K + 0] ^= prod[i];
-
-
-        
-           //128
+        }
+        if (K == 256)
+        {
+            // GF(2^256) from sage
+            prod[i - K + 10] ^= prod[i];
+            prod[i - K + 5] ^= prod[i];
+            prod[i - K + 2] ^= prod[i];
+            prod[i - K + 0] ^= prod[i];
+        }
+        if (K == 128)
+        {
+            // 128
             prod[i - K + 7] ^= prod[i];
             prod[i - K + 2] ^= prod[i];
             prod[i - K + 1] ^= prod[i];
             prod[i - K + 0] ^= prod[i];
-        */
-        /*
-        //x^64+1x^3+1x^1+37x^0
-            prod[i - K + 3] ^= prod[i];
-            prod[i - K + 1] ^= prod[i];
-            prod[i - K + 0] ^= gf_mul(prod[i], (unsigned short) 2);
-        */
-        /*
-        //32
+        }
+        if (K == 32)
+        {
+            // 32
             prod[i - K + 15] ^= prod[i];
             prod[i - K + 9] ^= prod[i];
             prod[i - K + 7] ^= prod[i];
             prod[i - K + 4] ^= prod[i];
             prod[i - K + 3] ^= prod[i];
             prod[i - K + 0] ^= prod[i];
-        */
-        
-        //16
+        }
+        if (K == 16)
+        {
+            // 16
             prod[i - K + 5] ^= prod[i];
             prod[i - K + 3] ^= prod[i];
             prod[i - K + 2] ^= prod[i];
             prod[i - K + 0] ^= prod[i];
-        
+        }
+        if (K == 8)
+        {
+            // 8
+            prod[i - K + 4] ^= prod[i];
+            prod[i - K + 3] ^= prod[i];
+            prod[i - K + 2] ^= prod[i];
+            prod[i - K + 0] ^= prod[i];
+        }
+        if (K == 4)
+        {
+            // 4
+            prod[i - K + 1] ^= prod[i];
+            prod[i - K + 0] ^= prod[i];
+        }
     }
 
     for (i = 0; i < K; i++)
         out[i] = prod[i];
 }
+
 
 // #define NN 16
 vec renritu(MTX a)
